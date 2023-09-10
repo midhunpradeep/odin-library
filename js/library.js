ced3f386a37b83906f58f5508e0d12f63b2f0043
Midhun Pradeep Nair <52858999+midhunpradeep@users.noreply.github.com>
2023-09-10 02:14:25 +0100

    fix: Make books show up as HTML elements

--- js/library.js
+++ js/library.js
@@ -1,22 +1,71 @@
 "use strict";
 
-function Library() {
+let idCounter = 0;
+function getNewID() {
+  let id = idCounter;
+  idCounter += 1;
+  return id;
+}
+
+function Library(htmlID) {
+  this.htmlID = htmlID;
   this.books = [];
 }
 
 Library.prototype.addBook = function (book) {
   this.books.push(book);
+
+  const htmlLibrary = document.getElementById(this.htmlID);
+  htmlLibrary.appendChild(book.generateBookHTMLElement());
 };
 
 function Book(name, author, numPages, hasBeenRead) {
+  this.id = getNewID();
   this.name = name;
   this.author = author;
   this.numPages = numPages;
   this.hasBeenRead = hasBeenRead;
 }
 
+Book.prototype.generateBookHTMLElement = function () {
+  const bookContainer = document.createElement("div");
+  bookContainer.classList.add("book-container");
+
+  const bookName = document.createElement("h2");
+  bookContainer.appendChild(bookName);
+  bookName.classList.add("book-name");
+  bookName.textContent = this.name;
+
+  const bookAuthor = document.createElement("h3");
+  bookContainer.appendChild(bookAuthor);
+  bookAuthor.classList.add("book-author");
+  bookAuthor.textContent = this.author;
+
+  const bookPages = document.createElement("div");
+  bookContainer.appendChild(bookPages);
+  bookPages.classList.add("book-pages");
+  bookPages.textContent = this.numPages + " Pages";
+
+  const bookReadLabel = document.createElement("label");
+  bookContainer.appendChild(bookReadLabel);
+  bookReadLabel.classList.add("book-read-box-id");
+  bookReadLabel.textContent = "Finished";
+
+  const bookRead = document.createElement("input");
+  bookContainer.appendChild(bookRead);
+  bookRead.classList.add("book-read-box");
+  bookRead.id = "book-" + this.id + "-read-box";
+  bookRead.type = "checkbox";
+  bookRead.checked = this.hasBeenRead;
+
+  bookReadLabel.htmlFor = bookRead.id;
+
+  return bookContainer;
+};
+
 function main() {
-  const myLibrary = new Library();
+  const myLibrary = new Library("library");
+
   myLibrary.addBook(new Book("The Alchemist", "Paulo Coelho", 167, true));
   myLibrary.addBook(
     new Book(
