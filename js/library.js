dac9f935460531347e057f07a91b7b93472f2a52
Midhun Pradeep Nair <52858999+midhunpradeep@users.noreply.github.com>
2023-09-10 01:49:03 +0100

    fix: Add library functions

--- js/library.js
+++ js/library.js
@@ -1,1 +1,39 @@
 "use strict";
+
+function Library() {
+  this.books = [];
+}
+
+Library.prototype.addBook = function (book) {
+  this.books.push(book);
+};
+
+function Book(name, author, numPages, hasBeenRead) {
+  this.name = name;
+  this.author = author;
+  this.numPages = numPages;
+  this.hasBeenRead = hasBeenRead;
+}
+
+function main() {
+  const myLibrary = new Library();
+  myLibrary.addBook(new Book("The Alchemist", "Paulo Coelho", 167, true));
+  myLibrary.addBook(
+    new Book(
+      "The Hitchhiker’s Guide To The Galaxy",
+      "Dougals Adams",
+      180,
+      true,
+    ),
+  );
+
+  myLibrary.addBook(
+    new Book("The Great Gatsby", "F. Scott Fitzgerald", 184, false),
+  );
+  myLibrary.addBook(new Book("The Call Of The Wild", "Jack London", 288, true));
+  myLibrary.addBook(new Book("To Kill A Mockingbird", "Harper Lee", 309, true));
+
+  console.log(myLibrary);
+}
+
+main();
